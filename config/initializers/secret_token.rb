12929c17243ab9234cb0caff4bb79e6e0c58c44c
Manish Puri <manishspuri@gmail.com>
2012-06-19 14:48:24 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetsWorld::Application.config.secret_token = 'aab4097a092c415a87f4861df36225a80f3479619e29cd08dfd4c9f9bb13432e5f475dd51e211c2ae7d3ca888b6214044f178d4d3bf9ee8b4a93af13fc385ae4'
